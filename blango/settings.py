d91b0a82773786c8d2b0a64fb86788acd0115080
Mario E. Salvatierra  Mario E. Salvatierra <mesalvav@yahoo.com>
2022-07-16 15:24:26 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -52,9 +52,11 @@ class Dev(Configuration):
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
+      'debug_toolbar',
   ]
 
   MIDDLEWARE = [
+      'debug_toolbar.middleware.DebugToolbarMiddleware',
       'django.middleware.security.SecurityMiddleware',
       'django.contrib.sessions.middleware.SessionMiddleware',
       'django.middleware.common.CommonMiddleware',
@@ -187,4 +189,5 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
   ]
-  
+
+  INTERNAL_IPS = ["192.168.10.226"]
