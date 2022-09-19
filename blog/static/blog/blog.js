8b424a12c2451557fcc208f4413c07f3c98e6f22
Mario E. Salvatierra  Mario E. Salvatierra <mesalvav@yahoo.com>
2022-09-19 18:29:58 +0000

    Finish Javascript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,45 +1,25 @@
-function sayHello(yourName) {
-  if (yourName === undefined) {
-      console.log('Hello, no name')
-  } else {
-       console.log('Hello, ' + yourName)
+function resolvedCallback(data) {
+  console.log('Resolved with data ' +  data)
+}
+
+function rejectedCallback(message) {
+  console.log('Rejected with message ' + message)
+}
+
+const lazyAdd = function (a, b) {
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("a and b must both be numbers")
+    } else {
+      const sum = a + b
+      resolve(sum)
+    }
   }
-}
-for(let i = 0; i < 10; i += 1) {
-  console.log('for loop i: ' + i)
-}
 
-let j = 0
-while(j < 10) {
-  console.log('while loop j: ' + j)
-  j += 1
+  return new Promise(doAdd)
 }
 
-let k = 10
+const p = lazyAdd(3, 4)
+p.then(resolvedCallback, rejectedCallback)
 
-do {
-  console.log('do while k: ' + k)
-} while(k < 10)
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-numbers.forEach((value => {
-  console.log('For each value ' + value)
-}))
-
-const doubled = numbers.map(value => value * 2)
-
-console.log('Here are the doubled numbers')
-
-console.log(doubled)
-
-const yourName = 'Your Name'  // Put your name here
-
-console.log('Before setTimeout')
-
-setTimeout(() => {
-    sayHello(yourName)
-  }, 2000
-)
-
-console.log('After setTimeout')
+lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
